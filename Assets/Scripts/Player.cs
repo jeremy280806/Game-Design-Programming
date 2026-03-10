def2ba690eda0bce417190db9dbd439f31188082
WKWKWK <jeremyarden2808@gmail.com>
2026-03-10 14:17:23 +0700

    Project Start
    Masih menambah Script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour {   
+    public Rigidbody2D rb;
+    public float jumpHeight = 7f;
+    public float moveSpeed = 5f;
+    private float movement; 
+    private bool isGround; 
+
+    public Transform groundCheckPoint;
+    public float groundCheckRadius = .2f;
+    public LayerMask whatIsGround;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start(){
+        isGround = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       movement = Input.GetAxis("Horizontal");
+
+       if(Input.GetKeyDown(KeyCode.Space)) {
+            Jump();
+         }
+        Collider2D collInfo = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, whatIsGround);
+
+        if(collInfo == true){
+            Debug.Log("Player is on Ground");
+        }
+    }
+    private void FixedUpdate() {
+        transform.position += new Vector3(movement * moveSpeed, 0f, 0f) * Time.fixedDeltaTime;
+    }
+
+    void Jump() {
+        if(isGround == true) {
+            Vector2 velocity = rb.linearVelocity;
+            velocity.y = jumpHeight;
+            rb.linearVelocity = velocity;
+            isGround = false;
+        }   
+    }
+}
